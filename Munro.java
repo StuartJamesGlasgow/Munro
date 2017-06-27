616037e4f696021bde3917bf762ed883df5c26b6
StuartJamesGlasgow <StuartJamesGlasgow@gmail.com>
2017-06-27 19:58:24 +0100

    Create Munro.java

---
+++ Munro.java
@@ -0,0 +1,23 @@
+package com.example.android.munros;
+
+/**
+ * Created by Stuart on 27/06/2017.
+ */
+
+public class Munro
+{
+    public String name;
+    public int height;
+
+    public Munro(String aName, int aHeight)
+    {
+        name = aName;
+        height = aHeight;
+    }
+
+
+
+    public void display()
+    {
+    }
+}
